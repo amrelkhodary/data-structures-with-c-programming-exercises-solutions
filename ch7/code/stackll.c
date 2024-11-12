68a26f1632af9f79c21be1279ae4a6ea8fe014f0
amrelkhodary <amrelkhodarybusiness@gmail.com>
2024-11-12 19:51:27 +0200

    created the skeleton for the stackll module

---
+++ ch7/code/stackll.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "stackll.h"
+
+Stackll* createStackll() {
+
+}
+
+int deleteStackll(Stackll* stackllptr) {
+
+}
+
+int push(Stackll* stackllptr, int value) {
+
+}
+
+int pop(Stackll* stackllptr) {
+
+}
+
+int get(Stackll* stackllptr) {
+
+}
+
+void debug_printStackll(Stackll* stackllptr) {
+
+}
