5dcd29a26aff6ee9dcb6d33c555aeab02c903238
amrelkhodary <amrelkhodarybusiness@gmail.com>
2024-11-13 13:57:37 +0200

    created createStackll function in stackll.c

---
+++ ch7/src/stackll.c
@@ -0,0 +1,34 @@
+#include "../headers/stackll.h"
+
+int main() {
+
+}
+
+Stackll* createStackll() {
+    Stackll* nstackll = (Stackll*) malloc(sizeof(Stackll));
+    if(nstackll == NULL) {
+        printf("MEMORY ERROR: Failed to allocate memory for a new linkedlist-based stack.\n");
+        return MEMORY_ERROR;
+    }
+
+    nstackll -> sdlinkedlist = createDoublyLinkedList();
+    nstackll -> current = NULL;
+    nstackll -> size = 0;
+
+    return nstackll;
+}
+int deleteStackll(Stackll* stackllptr) {
+
+}
+int push(Stackll* stackllptr, int element) {
+
+}
+int pop(Stackll* stackllptr) {
+
+}
+int get(Stackll* stackllptr) {
+
+}
+void debug_printStackll(Stackll* stackllptr) {
+
+}
