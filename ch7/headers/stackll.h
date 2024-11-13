f9d6c47a0b1718337873d56ca85a3bfbdce043b1
amrelkhodary <amrelkhodarybusiness@gmail.com>
2024-11-13 13:54:03 +0200

    added stackll header file

--- ch7/headers/stackll.h
+++ ch7/headers/stackll.h
@@ -11,3 +11,51 @@
     #include <stdlib.h>
     #include <stdbool.h>
 #endif
+
+#ifndef DOUBLY_LINKED_LIST_H
+ #define DOULBY_LINKED_LIST_H
+
+ #define TO_INSERT 10
+ #define TO_UPDATE_OR_DELETE 20
+
+ typedef struct Node {
+     int value;
+     struct Node* prev;
+     struct Node* next;
+ } Node;
+ 
+ typedef struct DoublyLinkedList {
+     Node* start;
+     Node* end;
+     int count;
+ } DoublyLinkedList;
+ 
+ DoublyLinkedList* createDoublyLinkedList();
+ int deleteDoublyLinkedList(DoublyLinkedList* dlinkedlist);
+ int insertNode(DoublyLinkedList* dlinkedlist, int position, int value);
+ int updateNode(DoublyLinkedList* dlinkedlist, int position, int newValue);
+ int deleteNode(DoublyLinkedList* dlinkedlist, int position);
+ 
+ bool positionIsValid(int position, int objective, DoublyLinkedList* dlinkedlist);
+ void printDoublyLinkedList(DoublyLinkedList* dlinkedlist);
+
+#endif
+
+
+#ifndef STACKLL_H
+    #define STACKLL_H
+
+    typedef struct Stackll {
+        DoublyLinkedList* sdlinkedlist;
+        Node* current;
+        int size;
+    } Stackll;
+
+    Stackll* createStackll();
+    int deleteStackll(Stackll* stackllptr);
+    int push(Stackll* stackllptr, int element);
+    int pop(Stackll* stackllptr);
+    int get(Stackll* stackllptr);
+    void debug_printStackll(Stackll* stackllptr);
+
+#endif
