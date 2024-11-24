f5ce08842d9015b5ce4c1e64fb032bac3e78f3ed
amrelkhodary <amrelkhodarybusiness@gmail.com>
2024-11-24 15:26:30 +0200

    wrote deleteQueue function inside queue.c

--- ch8/src/queue.c
+++ ch8/src/queue.c
@@ -7,10 +7,24 @@ int main()
 
 Queue* createQueue(int size) {
     Queue* nQueuePtr = (Queue*) malloc(sizeof(Queue));
+    if(nQueuePtr == NULL) {
+        printf("MEMORY_ERROR: failed to allocate memory to new queue.\n");
+        return MEMORY_ERROR;
+    }
     nQueuePtr -> elements = (int*) malloc(size * sizeof(int));
+    if(nQueuePtr -> elements == NULL) {
+        printf("MEMORY_ERROR: failed to allocate memory for elements array inside new queue.\n");
+        return MEMORY_ERROR;
+    }
     nQueuePtr -> pushindex = 0;
     nQueuePtr -> popindex = 0;
     nQueuePtr -> size = 0; //here, size referes to how many elements are currently in the queue not the size of the array itself
 
     return nQueuePtr;
 }
+
+int deleteQueue(Queue* queueptr) {
+    free(queueptr -> elements);
+    free(queueptr);
+}
+
