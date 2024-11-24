3f6e7dc944ec247224c450de4bad65443ea6bae5
amrelkhodary <amrelkhodarybusiness@gmail.com>
2024-11-24 15:19:48 +0200

    created queue.h

---
+++ ch8/headers/queue.h
@@ -0,0 +1,27 @@
+#ifndef STD 
+    #define STD
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <stdbool.h>
+#endif
+
+#ifndef ERRORS
+    #define MEMORY_ERROR 100;
+    #define INPUT_ERROR 103;
+#endif
+
+#ifndef QUEUE_H
+    typedef struct Queue {
+        int* elements;
+        int pushindex;
+        int popindex;
+        int size;
+    } Queue;
+
+    Queue* createQueue();
+    int deleteQueue(Queue* queueptr);
+    int push(Queue* queptr, int element);
+    int pop(Queue* queptr);
+    int get(Queue queptr);
+    void debugPrintQueue(Queue* queptr);
+#endif
